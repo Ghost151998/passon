f5db66413896850e5d68d46674e8fdbc6f675c7f
Siddhant Sinha <siddhantsinha140@gmail.com>
2018-03-17 13:38:45 +0530

    Cart implemented.Buttons remain to be handled.
    basic cart implemented.linking and buttons for submission and update of database upon checkout remains

--- cart.php
+++ cart.php
@@ -1,20 +1,22 @@
 <!-- User's Cart Page-->
-<!-- REMOVE ITEM TO BE ADDED FOR EACH ITEM,PRICES TO BE ADDED,BIKES AND MISC SHOULD BE HANDLED AND CHECKOUT TO BE HANDLED .Also to return to this page after removal of item-->
+<!-- REMOVE ITEM TO BE ADDED FOR EACH ITEM. CHECKOUT TO BE HANDLED .Also to return to this page after removal of item. ALIGN PRICE TO RIGHT. -->
+<!-- SUGGESTION : UPDATE AFTER 30 MINS -->
 <?php
 	session_start();
 	include ("dbconfig.php");//Connection to database
 
-	$redirect_to_login = "user_login.php";
+	$redirect_to_user_login = "user_login.php";
 
 	//Check if user is logged in
 	if(!$_SESSION["user_reg"]){//Login failed.Redirect to user_login.php
-		header("Location: " .$redirect_to_login);
+		header("Location: " .$redirect_to_user_login);
+
+		$cart_total = 0;
 	}
 
 	else{
 ?>
 
-			<!DOCTYPE html>
 			<html>
 				<head>
 					<title>Your Cart</title>
@@ -25,62 +27,65 @@
 						<thead>
 							<h3>Your Cart</h3>
 							<tr>
-								<th>Item Name</th>
-								<!-- <th>Price</th> -->
+								<th>Item</th>
+								<th>Price</th>
 							</tr>
 						</thead>
 						<tbody>
 							<?php
-								$books = mysqli_query($conn,"SELECT books.id, books.author_edition FROM books LEFT JOIN cart ON books.id = cart.item_id WHERE cart.reg = '".$_SESSION['user_reg']."'");//Query for books added to the cart by this user . 'books.price' to be added later
+								$books = mysqli_query($conn,"SELECT books.id, books.author, books.title, books.price FROM books INNER JOIN cart ON books.id = cart.item_id WHERE item_category = 'books' AND is_sold = 0 AND cart.reg = '".$_SESSION['user_reg']."'");//Query for books added to the cart by this user . 'books.price' to be added later
 
-								while($row = mysqli_fetch_array($books)) {//Start a while loop here to display fetch items in this division?>
+								while($row = mysqli_fetch_assoc($books)) {//Start a while loop here to display fetch items in this division?>
 									<tr>
 										<td>
-											<?php echo $row['author_edition'] ?>
+											<?php echo $row['author']." ".$row['title'] ?>
 										</td>
-										<!-- <td>
-											<?php ?>Price!
-										</td> -->
+										<td>
+											<?php echo $row['price'] ?>
+										</td>
 									</tr>
 								<?php 
+								global $cart_total;
+								$cart_total = $cart_total + $row['price'];
 								} 
+								
+								$bikes = mysqli_query($conn,"SELECT bikes.id, bikes.brand, bikes.gear, bikes.price FROM bikes INNER JOIN cart ON bikes.id = cart.item_id WHERE item_category = 'bikes' AND is_sold = 0 AND cart.reg = '".$_SESSION['user_reg']."'");//Query for bikes added to the cart by this user . 'bikes.price' to be added later
+							
+								while($row = mysqli_fetch_assoc($bikes)) {//Start a while loop here to display fetch items in this division?>
+									<tr>
+										<td>
+											<?php
+											echo $row['brand']." ".$row['gear'] ?>
+										</td>
+										<td>
+											<?php echo $row['price'] ?>
+										</td>
+									</tr>
+								<?php 
+								global $cart_total;
+								$cart_total = $cart_total + $row['price'];
+								}
+
+								$misc = mysqli_query($conn,"SELECT misc.id, misc.name, misc.price FROM misc INNER JOIN cart ON misc.id = cart.item_id WHERE item_category = 'misc' AND is_sold = 0 AND cart.reg = '".$_SESSION['user_reg']."'");//Query for misc added to the cart by this user . 'misc.price' to be added later
+							
+								while($row = mysqli_fetch_assoc($misc)) {//Start a while loop here to display fetch items in this division?>
+									<tr>
+										<td>
+											<?php echo $row['name'] ?>
+										</td>
+										<td>
+											<?php echo $row['price'] ?>
+										</td>
+									</tr>
+								<?php 
+								global $cart_total;
+								$cart_total = $cart_total + $row['price'];
+								}
 								?>
-
-							<!-- <?php
-								$bikes = mysqli_query($conn,"SELECT bikes.id, bikes.brand, bikes.gear FROM bikes LEFT JOIN cart ON bikes.id = cart.item_id WHERE cart.reg = '".$_SESSION['user_reg']."'");//Query for bikes added to the cart by this user . 'bikes.price' to be added later
-							
-								while($row = mysqli_fetch_array($bikes)) {//Start a while loop here to display fetch items in this division?>
-									<tr>
-										<td>
-											<?php 
-											$name = $row['brand']." ".$row['gear'];
-											echo $name ?>
-										</td>
-										<td>
-											<?php ?>Price!
-										</td>
-									</tr>
-								<?php 
-								} 
-								?> -->
-
-							<!-- <?php
-								$misc = mysqli_query($conn,"SELECT misc.id, misc.item_name FROM misc LEFT JOIN cart ON misc.id = cart.item_id WHERE cart.reg = '".$_SESSION['user_reg']."'");//Query for misc added to the cart by this user . 'misc.price' to be added later
-							
-								while($row = mysqli_fetch_array($misc)) {//Start a while loop here to display fetch items in this division?>
-									<tr>
-										<td>
-											<?php echo $row['item_name'] ?>
-										</td>
-										<td>
-											<?php ?>Price!
-										</td>
-									</tr>
-								<?php 
-								} 
-								?> -->
 						</tbody>
 					</table>
+					<hr>
+					Cart Total :<?php echo $cart_total; ?>
 				</body>
 			</html>
 
